e0df1a6772aba874da4994da28524f50a46304ce
jane kaiser <37499394+segajanesis@users.noreply.github.com>
2021-03-02 11:39:45 -0800

    Upload Tavern.sh tutorial script

---
+++ Tavern.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Welcome in, stranger! What's your name?"
+read PERSON
+echo "Hello, $PERSON! I'm so glad you stopped by today. Where are you from?"
+read LOCATION
+echo "Wow, all the way from $LOCATION! We don't get too many visitors from there. Say, what will you be drinking today?"
+read DRINK
+echo "A fine choice! One $DRINK, coming up. This one's on the house. Thanks again for stopping in, $PERSON!"
